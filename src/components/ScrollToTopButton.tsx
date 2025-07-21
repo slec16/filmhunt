3efcef786d93b46cb8984b597321e39cccd1b376
slec16 <sankopytov.76@mail.ru>
2025-07-22 02:00:13 +0300

    hotfix

--- src/components/ScrollToTopButton.tsx
+++ src/components/ScrollToTopButton.tsx
@@ -30,7 +30,7 @@ const ScrollToTopButton = () => {
         <button
             onClick={scrollToTop}
             className={`fixed bottom-10 p-3 bg-orange-500 text-white rounded-full shadow-lg transition-all duration-300 ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'
-                } hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-orange-400 focus:ring-opacity-50`}
+                } hover:bg-orange-700 `}
         >
             <ArrowUpwardIcon />
         </button>
