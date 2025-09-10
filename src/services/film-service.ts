58e9d03f986ace1c2118b095646aafc3a10352e1
slec16 <sankopytov.76@mail.ru>
2025-09-11 01:24:46 +0300

    feat: film service

--- src/services/film-service.ts
+++ src/services/film-service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, MemoryCache } from "./http-service"
 
 type FetchOptions = {
-    signal?: AbortSignal,
+    signal: AbortSignal,
     timeoutMs?: number,
     retry?: number,
     retryDelayMs?: number
@@ -15,17 +15,16 @@ class FilmService {
     private baseParams = 'movie?notNullFields=id&notNullFields=name&notNullFields=shortDescription&notNullFields=year&notNullFields=rating.imdb&notNullFields=ageRating&notNullFields=genres.name&notNullFields=poster.url'
 
 
-    async getFilmsByFilter(page: number, limit: number, paramsPath?: string, options?: FetchOptions) {
+    async getFilmsByFilter(page: number, limit: number, options: FetchOptions, paramsPath?: string) {
 
         const path = this.baseParams + `&page=${page}&limit=${limit}` + paramsPath
         const key = buildKey(path)
 
         const cached = this.cache.get<any>(key)
-        // свежие данные сразу возвращаем; при желании можно фоном обновлять
-        if (cached && !this.cache.isStale(key)) return cached;
+        // свежие данные сразу возвращаем,  можно фоном обновлять
+        if (cached && !this.cache.isStale(key)) return cached
 
-        //@ts-ignore
-        const data = await this.http.newGet(path, options)
+        const data = await this.http.get(path, options)
 
         //TODO добавить нормализацию
 
@@ -33,6 +32,74 @@ class FilmService {
         return data
 
     }
+
+    async getFilmBySearch(page: number, limit: number, name: string, options: FetchOptions) {
+        const path = `movie/search?page=${page}&limit=${limit}&query=${name}`
+        const key = buildKey(path)
+        const cached = this.cache.get<any>(key)
+
+        if (cached && !this.cache.isStale(key)) return cached
+
+        const data = await this.http.get(path, options)
+
+        this.cache.set(key, data)
+        return data
+
+    }
+
+    async getFilmById(id: string, options: FetchOptions) {
+        const path = `movie/${id}`
+        const key = buildKey(path)
+        const cached = this.cache.get<any>(key)
+
+        if (cached && !this.cache.isStale(key)) return cached
+
+        const data = await this.http.get(path, options)
+
+        this.cache.set(key, data)
+        return data
+    }
+
+    async getSeasonsById(id: string, options: FetchOptions) {
+        const path = `season?page=1&limit=10&movieId=${id}`
+        const key = buildKey(path)
+        const cached = this.cache.get<any>(key)
+
+        if (cached && !this.cache.isStale(key)) return cached
+
+        const data = await this.http.get(path, options)
+
+        this.cache.set(key, data)
+        return data
+    }
+
+    async getReviewByFilmId(id: string, page: string, limit: string, options: FetchOptions) {
+        const path = `review?page=${page}&limit=${limit}&movieId=${id}`
+        const key = buildKey(path)
+        const cached = this.cache.get<any>(key)
+
+        if (cached && !this.cache.isStale(key)) return cached
+
+        const data = await this.http.get(path, options)
+
+        this.cache.set(key, data)
+        return data
+    }
+
+    async getRandomFilm(year: string, genres: string, countries: string, options: FetchOptions) {
+        const path = `movie/random?notNullFields=name&notNullFields=shortDescription&year=${year}${genres}${countries}`
+        const key = buildKey(path)
+        const cached = this.cache.get<any>(key)
+
+        if (cached && !this.cache.isStale(key)) return cached
+
+        const data = await this.http.get(path, options)
+
+        this.cache.set(key, data)
+    }
+
+    //TODO - invalidate, refresh
+
 }
 
 export default new FilmService(new HttpClient, new MemoryCache(300000, 100000))
